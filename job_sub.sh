495ae3d86913ce4484b8ff30e554b43c5cb55569
ael-code <tommy.ael@gmail.com>
2016-10-22 19:58:57 +0200

    added submission script

---
+++ job_sub.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+job_template(){
+
+    cat <<-EOF
+		#BSUB -J ${name="NAME"}
+		#BSUB -oo ${out_folder:-"out/"}${name}_%J.out
+
+		#BSUB -q ${queue:-"debug"}
+		#BSUB -W ${time_limit:-"00:01"}
+
+		#BSUB -n ${pnum:-1}
+		#BSUB -R"span[ptile=${p4node:-$pnum}]"
+		#BSUB -x
+
+		${exe:-"CMD_TO_EXE"}
+	EOF
+}
+
+name="job_name"
+exe="echo 'some heavy work'"
+#pnum=1
+#p4node=1
+#queue="debug"
+#time_limit="00:01"
+
+
+#for i in 1 2 4 8 16; do
+#    for j in `seq 1`; do
+		if [ -z ${TEST} ]; then
+			job_template | bsub
+		else
+			job_template
+		fi
+#    done
+#done
